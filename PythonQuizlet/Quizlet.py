df7c7b92b562aa549c710d9ca95d73fb0ae58930
Jacob Greene <35856498+greenej10@users.noreply.github.com>
2018-04-11 21:25:51 -0400

    Quizlet
    Quizlet

--- PythonQuizlet/Quizlet.py
+++ PythonQuizlet/Quizlet.py
@@ -1,5 +1,6 @@
 from tkinter import *
-# keep the question in a separate json file
+from tkinter import messagebox
+
 questions = [
     "Q1: What is the programming language we are using?",
     "Q2: What are the main data structures of Python?",
@@ -7,19 +8,29 @@ questions = [
 
 options = [
     ["Java", "Ruby ", "Python", "Perl"],
-    ["Delhi", "Mumbai", "Chennai", "Kanyakumari"],
+    ["Delhi", "Mumbai", "Chennai", "List"],
 ]
 
-a = [3, 4]
+answers = [3, 4]
+
+descriptions = [
+                "We are using Python for this project.",
+                "Lists are a data structure in Python"
+                ]
+
+
 
 class Quizlet:
     def __init__(self, master):
+
         self.answer_choice = IntVar()
         self.question_number = 0
         self.num_correct = 0
         self.question = self.question_maker(master, self.question_number)
         self.options = self.option_maker(master, 4)
         self.show_question(self.question_number)
+        self.description= Message(master, text="")
+        self.description.pack()
         self.button = Button(master, text="Back", command=self.back_btn)
         self.button.pack(side=BOTTOM)
         self.button = Button(master, text="Next", command=self.next_btn)
@@ -32,49 +43,70 @@ class Quizlet:
         return prompt
 
     def option_maker(self, master, n):
-        b_val = 0
-        b = []
-        while b_val < n:
-            btn = Radiobutton(master, text="foo", variable=self.answer_choice, value=b_val + 1)
-            b.append(btn)
-            btn.pack(side=TOP, anchor="w")
-            b_val = b_val + 1
-        return b
+        button_value = 0
+        button_list = []
+        while button_value < n:
+            button = Radiobutton(master, text="", variable=self.answer_choice, value=button_value + 1)
+            button_list.append(button)
+            button.pack(side=TOP, anchor="w")
+            button_value = button_value + 1
+        return button_list
+
+
+    def answer_maker(self, master, answer, description):
+        prompt = Label(master, text=answers[answer])
+        prompt.pack(side=TOP)
+        prompt.config(font=("Courier", 16))
+        description= Message(master, text= descriptions[description])
+        description.pack()
+        description.config(font=("Courier",12))
+        return prompt
+
+    def description_maker(self, master):
+        self.description.config(text = descriptions[self.question_number], font=("Courier",12))
+
 
     def show_question(self, question):
-        b_val = 0
+        self.question['text'] = questions[question]
+        for option in options[question]:
+            self.options[button_value]['text'] = option
+            button_value = button_value + 1
+
+    def show_question(self, question):
+        button_value = 0
         self.answer_choice.set(0)
         self.question['text'] = questions[question]
         for option in options[question]:
-            self.options[b_val]['text'] = option
-            b_val = b_val + 1
+            self.options[button_value]['text'] = option
+            button_value = button_value + 1
 
-    def check_q(self, qn):
-        if self.answer_choice.get() == a[qn]:
+    def answer_check(self, question):
+        if self.answer_choice.get() == answers[question]:
             return True
         return False
 
     def print_results(self):
-        print("Score: ", self.num_correct, "/", len(questions))
-
+        messagebox.showinfo("Score: ", "You got : " + str(self.num_correct) + "/" + str(len(questions)))
     def back_btn(self):
-        print("go back")
+        self.description_maker(root)
 
     def next_btn(self):
-        if self.check_q(self.question_number):
+        if self.answer_check(self.question_number):
             print("Correct")
             self.num_correct += 1
+            self.description.config(text="")
         else:
             print("Wrong")
         self.question_number = self.question_number + 1
         if self.question_number >= len(questions):
             self.print_results()
+            quit()
         else:
             self.show_question(self.question_number)
 
 
 root = Tk()
-root.geometry("800x800")
+root.geometry("800x600")
 menu= Menu(root)
 logo =  PhotoImage(file="python-logo.png")
 title = Label(root, image=logo)
